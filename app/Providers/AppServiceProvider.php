0592c64c412ec734bc3c0722dade0c913f130748
Kawan Kakubo <kawanhrs@gmail.com>
2026-08-14 05:56:54 -0300

    feat: force https scheme in AppServiceProvider for secure URL generation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use App\Models\WomenClinicAppointment;
 use App\Models\CentralPharmacyRequest;
 use App\Observers\WomenClinicAppointmentObserver;
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        URL::forceScheme('https');
         WomenClinicAppointment::observe(WomenClinicAppointmentObserver::class);
         CentralPharmacyRequest::observe(CentralPharmacyRequestObserver::class);
     }
